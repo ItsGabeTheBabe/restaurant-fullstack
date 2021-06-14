fa9a012c0e70e0c396cecf2d66a9e16b2ca5f70d
Gabe <gabrieltorres.dev@gmail.com>
2021-06-13 18:20:18 -0700

    fixing dashboard values

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -56,7 +56,7 @@ class AdminController extends Controller
                 'estimated_income_last_30' => "$" . $estimated_income_last_30[0]->total,
                 'total_customers_last_30' => $total_customers_last_30[0]->total,
                 'total_reservations_last_30' => $total_reservations_last_30[0]->total,
-                'total_employees' => "$" . $total_employees[0]->total,
+                'total_employees' => $total_employees[0]->total,
                 'recentReservations' => $recentReservations
             ]
         );
@@ -74,12 +74,11 @@ class AdminController extends Controller
         //     group by reserved_day desc
         // '));
         return $estimated_income_daily_data = DB::select(DB::raw('
-        SELECT 
-                DATE_FORMAT(created_at,"%Y-%m-%d") as x,
-                (sum(guest_total)*27) as y
+            SELECT 
+                DATE_FORMAT(created_at,"%Y-%m-%d") as x, 
+                (sum(guests_total) * 27 ) as y
             FROM reservations
-            GROUP BY x 
-            ORDER BY x DESC;
+            group by x desc;
         '));
     }
 }
