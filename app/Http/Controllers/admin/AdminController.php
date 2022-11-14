63ec21f40be767c932c934fd606d2ea6df869968
ItsGabeTheBabe <47126211+ItsGabeTheBabe@users.noreply.github.com>
2022-11-14 13:36:04 -0800

    Update AdminController.php

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -78,8 +78,7 @@ class AdminController extends Controller
                 DATE_FORMAT(created_at,"%Y-%m-%d") as x, 
                 (sum(guests_total) * 27 ) as y
             FROM reservations
-            group by x
-            ORDER BY x desc;
+            group by x;
         '));
     }
 }
